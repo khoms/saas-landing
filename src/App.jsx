e2e430d9879e82cbcb8b8bb2750104fd1abd535c
khoms <sundp.kshetri@gmail.com>
2024-08-31 12:54:05 +0545

    Initialize the project with required setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-red-400 p-10">Nepal</div>
+    </>
+  );
+}
+
+export default App;
